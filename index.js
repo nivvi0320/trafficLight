8e3321f701e63b80687b8d1ebc1d5d09e916a6ca
nivvi0320 <144514790+nivvi0320@users.noreply.github.com>
2024-07-25 22:04:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function stopFun() {
+    stopButton.style.backgroundColor = "#cf1124"
+    stopLight.style.backgroundColor = "#cf1124"
+    readyButton.style.backgroundColor = "#1f1d41"
+    readyLight.style.backgroundColor = "#4b5069"
+    goButton.style.backgroundColor = "#1f1d41"
+    goLight.style.backgroundColor = "#4b5069"
+
+}
+
+function readyFun() {
+    readyButton.style.backgroundColor = "#f7c948"
+    readyLight.style.backgroundColor = "#f7c948"
+    stopButton.style.backgroundColor = "#1f1d41"
+    stopLight.style.backgroundColor = "#4b5069"
+    goButton.style.backgroundColor = "#1f1d41"
+    goLight.style.backgroundColor = "#4b5069"
+
+}
+
+function goFun() {
+    goButton.style.backgroundColor = "#199473"
+    goLight.style.backgroundColor = "#199473"
+    stopButton.style.backgroundColor = "#1f1d41"
+    stopLight.style.backgroundColor = "#4b5069"
+    readyButton.style.backgroundColor = "#1f1d41"
+    readyLight.style.backgroundColor = "#4b5069"
+
+
+}
